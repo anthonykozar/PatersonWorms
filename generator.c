c592a06787dc56b8e8aa0585c37a883dbe91a910
Andrew McAdams <andmcadams@gmail.com>
2019-01-24 18:32:09 -0600

    Translate moveTo function from js file into move_to

--- generator.c
+++ generator.c
@@ -7,10 +7,50 @@
 /*Possible optimizations:
 	Use 6 bits for each point (requires allocation of 1 byte, could use a char)
 */
+#include <math.h>
+
+int DEBUG = 0;
+int DIR_MATRIX[6][2] = {{1, 0}, {1, -1}, {0, -1}, {-1, 0}, {-1, 1}, {0, 1}};
+int field_array[7] = {1, 2, 2, 1, 0, 2, 0};
+int choice1_f[2] = {3, 4};
+int choice2_f[4] = {0,1,2,3};
+int choice3t_f[3] = {0,1,2};
+int choice3b1_f[3] = {2,0,1};
+int choice3b2_f[3] = {1,0,2};
+int choice3b3_f[3] = {1,0,2};
+int choice3b4_f[3] = {0, 1, 2};
+int choice4_f[2] = {0, 1};
+
+double snap_center_x;
+double snap_center_y;
+double line_length;
 
 typedef struct {
 	char edges;
 } point;
+
+int move_to(point** map, int c_x, int c_y, int x, int y, int to_dir, int step) {
+  if(DEBUG) {
+    printf("moveTo(%d, %d, %d, %d, %d)", c_x, c_y, x, y, to_dir);
+  };
+  
+  map[x][y].edges |= 1 << ((to_dir + 3) % 6);
+  map[c_x][c_y].edges |= 1 << (to_dir % 6);
+  
+  double x1 = snap_center_x;
+  double x2 = snap_center_x - line_length*cos(((to_dir + 3) % 6)*M_PI/3);
+  double y1 = snap_center_y;
+  double y2 = snap_center_y - line_length*sin(((to_dir + 3) % 6)*M_PI/3);
+
+  snap_center_x = x2;
+  snap_center_y = y2;
+
+  return 0;
+}
+
+void next_step() {
+
+}
 
 point** init_graph(int size) {
 	point** map = (point **) calloc(1, sizeof(point *)*size);
@@ -26,7 +66,7 @@ int main() {
 	int i,j;
 	// for(i = 0; i < size; i++) {
 	// 	for(j = 0; j < size; j++)
-	// 		printf("%c ", map[i][j].edges);
+	// 		printf("%c ", map{i}{j}.edges);
 	// 	printf("\n");
 	// }
 	printf("Total size: %d bytes\n", 8+size*8+size*size);
